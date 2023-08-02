7abc0dfc97bf772f38edec76eb410fe5a992552b
Xenon <x.vergara2201@gmail.com>
2023-08-03 00:14:38 +0800

    update informations

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -6,66 +6,25 @@ function Information() {
         <span className='bg-secondary h-2 w-[150px]'></span>
       </div>
       <p className='text-justify my-5 indent-8 leading-8'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga ipsa
-        repellendus distinctio in, perferendis eligendi sunt alias odit,
-        repudiandae facere quos quaerat saepe officia exercitationem eius magnam
-        maiores nostrum? Placeat dolorum laboriosam enim, commodi molestiae
-        nostrum repellendus adipisci saepe magni, fuga a tempore illum
-        blanditiis tenetur fugiat ut quisquam repellat soluta ipsum ex eius
-        deleniti nulla molestias doloribus? Eius quibusdam nihil iure,
-        perferendis numquam quis repudiandae et nam ipsa voluptatibus, aut amet
-        harum praesentium aspernatur velit itaque aperiam corrupti dicta,
-        corporis a reprehenderit. Dolorum, eum fugit nisi numquam eveniet
-        repellendus minus consectetur eius dicta voluptatem non cumque iusto ab
-        nulla eligendi sit ea! Odit, totam dicta dolore tempora quis, commodi
-        sapiente eius libero dolor voluptatum quod laborum eligendi mollitia,
-        ullam ab! Blanditiis, vero cumque. Quam inventore odit non quae quia.
+        IN-BESTMENT: Empowering Dreams, Fuelling Prosperity. Led by visionary
+        founder Emma Anderson, our investment company stands apart with a deep
+        commitment to our clients&apos; financial aspirations. We believe that
+        every individual deserves access to investment opportunities, and
+        that&apos;s why we craft personalized strategies that reflect unique
+        goals and risk tolerance. With a forward-thinking approach, we identify
+        emerging industries and disruptive technologies, making early
+        investments that generate remarkable returns while fostering a spirit of
+        innovation within our firm.
       </p>
       <p className='text-justify my-5 indent-8 leading-8'>
-        Optio deleniti ex minima corporis, incidunt nam molestiae cumque
-        veritatis quaerat dolor consequuntur harum velit doloribus vitae
-        repellendus voluptate architecto alias eum fuga quibusdam cum soluta!
-        Atque iusto iure minus delectus saepe in fugiat cupiditate laboriosam
-        cumque incidunt temporibus doloribus, ipsam rem perspiciatis possimus
-        aliquid, exercitationem voluptatibus soluta nihil, pariatur sed officiis
-        sunt id. Commodi fugit obcaecati suscipit voluptates officia eveniet
-        voluptatibus maiores cupiditate iure, sapiente dolor laboriosam
-        necessitatibus vitae, aperiam at perspiciatis officiis, quasi molestiae
-        doloribus dolores nemo! Quia autem sequi, consectetur velit numquam id
-        quae porro tempora dolores delectus ex eaque provident eveniet
-        molestiae, ipsum adipisci alias natus nemo rem nesciunt minima
-        consequatur. Asperiores, ad dolore amet obcaecati, totam veritatis
-        consectetur non debitis doloremque illum blanditiis saepe perferendis
-        repellendus dicta velit excepturi odio a earum necessitatibus. Numquam
-        veniam sit animi non alias mollitia esse tenetur repudiandae omnis cum
-        iste quia distinctio molestiae aperiam quam minima, in adipisci suscipit
-        odit quasi. Hic beatae, porro voluptates, adipisci alias nesciunt
-        aliquid earum vel aspernatur vero totam amet architecto asperiores
-        quisquam magnam similique esse, accusantium labore cupiditate temporibus
-        voluptatum! Iure impedit expedita quidem! Commodi earum, corporis qui at
-        fugiat eius saepe? Nulla, saepe. Sit voluptatem facere culpa alias quae
-        minus porro ea expedita tempora officiis blanditiis, itaque doloribus
-        maiores odit aspernatur! Repellat excepturi praesentium odio quis sed
-        exercitationem dolore eligendi recusandae nobis nam facere neque labore,
-        dicta vel voluptates voluptate. Commodi rem quod facilis, aut natus,
-        saepe temporibus, molestiae consequuntur dolorum dignissimos iure
-        architecto ratione! Assumenda delectus consequatur, corrupti sed nobis
-        maxime odio quae dolorum excepturi ullam earum, quisquam consequuntur
-        cum enim iste aperiam perspiciatis unde! Autem ad amet libero incidunt
-        id, qui sint esse! Modi accusantium possimus delectus, ipsam quo autem?
-      </p>
-      <p className='text-justify my-5 indent-8 leading-8'>
-        Id perferendis eaque placeat, neque labore earum adipisci vitae!
-        Reiciendis praesentium accusantium explicabo, incidunt temporibus
-        excepturi, ea veniam ipsam eligendi veritatis ut? Aspernatur culpa,
-        dolores adipisci ipsam non totam veritatis velit odit vero ipsa dolorum
-        officiis nobis? Nulla necessitatibus cupiditate reiciendis similique
-        unde nihil laudantium repellendus, vel voluptates quidem earum dolorem
-        maxime. Quasi, tenetur! Hic, distinctio officia corrupti aliquam, ipsam
-        dolorem doloribus est necessitatibus, dolor dicta consectetur cum
-        quaerat illum eos similique iste ad sunt deserunt quidem. Quam illo
-        tempore a accusantium distinctio, veritatis voluptatibus eveniet
-        suscipit facere. Facere, placeat.
+        Beyond financial success, we actively engage in socially responsible
+        investing, empowering clients to invest in projects that contribute to
+        the greater good. From supporting sustainable energy initiatives to
+        championing social enterprises, IN-BESTMENT is dedicated to creating a
+        positive impact on society. Our journey is one of trust, integrity, and
+        lasting legacies, leaving behind a world where financial dreams are
+        attainable for everyone. Trust us to unlock your financial potential and
+        embark on a rewarding journey to prosperity.
       </p>
     </div>
   );
